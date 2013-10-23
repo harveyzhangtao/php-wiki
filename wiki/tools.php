17f780d444362cd0fd5a2ba249aa50aeabc7b4f3
张涛 <harveyzhang@anjuke.com>
2013-10-23 13:59:55 +0800

    笔记

---
+++ wiki/tools.php
@@ -0,0 +1,22 @@
+<?php
+
+    /*
+     * 对二维数组排序
+     *
+     */
+    public function array_sort($arr,$keys,$type='asc'){
+        $keysvalue = $new_array = array();
+        foreach ($arr as $k=>$v){
+            $keysvalue[$k] = $v[$keys];
+        }
+        if($type == 'asc'){
+            asort($keysvalue);
+        }else{
+            arsort($keysvalue);
+        }
+        reset($keysvalue);
+        foreach ($keysvalue as $k=>$v){
+            $new_array[] = $arr[$k];
+        }
+        return $new_array;
+    }
