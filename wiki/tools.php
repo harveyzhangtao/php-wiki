bd24a148c980dbabf20b5128c5c71bd81842299c
harveyzhangtao <358465355@qq.com>
2016-02-02 13:48:13 +0800

    Update tools.php

--- wiki/tools.php
+++ wiki/tools.php
@@ -20,3 +20,36 @@
         }
         return $new_array;
     }
+
+
+//对二维数组排序
+  public function test(){
+        $test = array(
+            '0' => array(
+                'num' => 10,
+                'name' => 'test'
+            ),
+            '1' => array(
+                'num'  => 20,
+                'name' => 'test2'
+            ),
+            '2' => array(
+                'num'  => 1,
+                'name' => 'asasa'
+            ),
+            '3' => array(
+                'num' => 30,
+                'name' => 'sdad'
+            ),
+            '21' => array(
+                'num' => 2,
+                'name' => 'sdad'
+            )
+        );
+        $tmp = array();
+        foreach($test as $k=>$v){
+            $tmp[$k] = $v['num'];
+        }
+        array_multisort($test, SORT_DESC, SORT_STRING, $tmp, SORT_DESC);
+        var_dump($test);
+    }
